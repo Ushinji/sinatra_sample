9e30aa6cbafb83fbdd508dc1d4bc0f86d4ec9d1e
Ushinji <siguma971@gmail.com>
2018-06-04 22:04:13 +0900

    Userの新規作成機能を実装

--- myapp.rb
+++ myapp.rb
@@ -9,3 +9,11 @@ get '/users' do
   @users = User.all
   erb :'users/index'
 end
+
+post '/users' do
+  user = User.new
+  user.name = params[:name]
+  user.email = params[:email]
+  user.save
+  redirect '/users'
+end
